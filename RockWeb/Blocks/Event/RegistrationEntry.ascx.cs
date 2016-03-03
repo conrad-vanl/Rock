283363dd7eb319b469f57994ad6d5745ab0d1521
Mike Peterson <MikePeterson@ccvonline.com>
2016-03-02 17:45:40 -0700

    - fix exception that would occur in Registration Entry if the Registrar was anonymous/new

--- RockWeb/Blocks/Event/RegistrationEntry.ascx.cs
+++ RockWeb/Blocks/Event/RegistrationEntry.ascx.cs
@@ -2153,7 +2153,7 @@ namespace RockWeb.Blocks.Event
                                 registrantNames.Add( registrant.FullName );
                             }
 
-                            if ( registrar == null && ( RegistrationState.FirstName != registrar.NickName || RegistrationState.LastName != registrar.LastName ) )
+                            if ( registrar != null && ( RegistrationState.FirstName != registrar.NickName || RegistrationState.LastName != registrar.LastName ) )
                             {
                                 noteText.AppendFormat( " by {0}", RegistrationState.FirstName + " " + RegistrationState.LastName );
                             }
